0d1552f72b4030d06098da00f9cd0457123fc645
Anjorin Samson <88699664+otunbasoa@users.noreply.github.com>
2023-09-27 06:16:24 +0100

    #23 bucket should no longer use random provider (#24)

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,6 @@
  # value = random_string.bucket_name.result
 #}
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+output "bucket_name" {
+  value = aws_s3_bucket.website_bucket
 }
