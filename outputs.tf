2cecf507c3653433f2258a16186245c5d583aa3f
Anjorin Samson <anjorinolu.samson@gmail.com>
2023-09-27 06:25:57 +0000

    #25 refactor s3 bucket into the new terrahouse_aws module

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,4 @@
-#output "random_bucket_name" {
- # value = random_string.bucket_name.id
-#}
-#output "random_bucket_name_result" {
- # value = random_string.bucket_name.result
-#}
-
 output "bucket_name" {
-  value = aws_s3_bucket.website_bucket
+  description = "Bucket name for our static website hosting"
+  value = module.terrahouse_aws.bucket_name
 }
